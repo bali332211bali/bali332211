7415a4571df188bf6e3ccf00d90b548042b20b5a
Bali <email@email.com>
2018-10-29 12:27:12 +0100

    hellouser

---
+++ week-01/day-4/src/hellouser.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class hellouser {
+    public static void main(String[] args) {
+
+        // Modify this program to greet user instead of the World!
+        // The program should ask for the name of the user
+
+        System.out.println("name");
+        Scanner scanner = new Scanner(System.in);
+        String user = scanner.nextLine();
+
+
+        System.out.println("Hello, " + user);
+
+
+
+
+
+
+    }
+}
