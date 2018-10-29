6d7b83c9873cc489a880ecb01f2e0b8a89997bf5
Bali <email@email.com>
2018-10-29 13:55:40 +0100

    cheat

---
+++ week-01/day-4/src/cheat.java
@@ -0,0 +1,15 @@
+public class cheat {
+    public static void main(String[] args) {
+        // Create a program that writes this line 100 times:
+        // "I won't cheat on the exam!"
+        int a = 0;
+
+        while (a <= 99) {
+
+            a++;
+            System.out.println(a+ " cheat");
+        }
+
+
+    }
+}
