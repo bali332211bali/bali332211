546998c9fb8ab62ac19ccf477a5c394900d3eaf5
Bali <email@email.com>
2018-10-29 14:00:27 +0100

    even

---
+++ week-01/day-4/src/even.java
@@ -0,0 +1,12 @@
+public class even {
+    public static void main(String[] args) {
+        // Create a program that prints all the even numbers between 0 and 500
+
+        for (int a = 0; a <= 500; a++) {
+            if ((a != 0) && (a % 2 == 0)) {
+                System.out.println(a);
+            }
+        }
+
+    }
+}
