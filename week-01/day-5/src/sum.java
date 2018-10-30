749fb682efb3d626a5788fd89b3be7b6fd66d23f
Bali <email@email.com>
2018-10-30 12:34:57 +0100

    sum

---
+++ week-01/day-5/src/sum.java
@@ -0,0 +1,24 @@
+public class sum {
+
+
+    public static int sum(int a, int b) {
+
+
+       int sumab = a + b;
+        return sumab;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(sum(3, 4));
+    }
+
+    // - Write a function called `sum` that sum all the numbers
+    //   until the given parameter and returns with an integer
+
+
+}
+
+
