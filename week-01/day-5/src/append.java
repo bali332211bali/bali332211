46ba0c3622405c87a6315eab7f87a134d47cfc6c
Bali <email@email.com>
2018-10-30 12:26:49 +0100

    append

---
+++ week-01/day-5/src/append.java
@@ -0,0 +1,26 @@
+public class append {
+
+
+    public static String append(String str) {
+        str = str + "a";
+        return str;
+    }
+
+
+    public static void main(String[] args) {
+        String typo = "chinchill";
+
+        System.out.println(append(typo));
+    }
+
+
+    //  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a string variable named `typo` and assign the value `Chinchill` to it
+// - Write a function called `appendAFunc` that gets a string as an input,
+//   appends an 'a' character to its end and returns with a string
+//
+// - Print the result of `appendAFunc(typo)`
+
+}
