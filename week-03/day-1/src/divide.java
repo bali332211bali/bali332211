b822b3fbf1a9ea6ce74bd41fcf25cd96e14eef84
Bali <bali332211@freemail.hu>
2018-11-09 23:23:53 +0100

    divide

---
+++ week-03/day-1/src/divide.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class divide {
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("number");
+        Scanner scanner = new Scanner(System.in);
+        int user = scanner.nextInt();
+
+
+
+        try {
+            System.out.println(10/user);
+
+        } catch ( ArithmeticException e) {
+            System.out.println("can't");
+        }
+
+
+
+    }
+}
