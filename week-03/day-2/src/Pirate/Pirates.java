bbd1c9a927bfbf59fbd59869eab8103d0db056e1
Bali <bali332211@freemail.hu>
2018-11-13 20:47:31 +0100

    pirates

--- week-03/day-2/src/Pirate/Pirates.java
+++ week-03/day-2/src/Pirate/Pirates.java
@@ -4,15 +4,17 @@ public class Pirates {
 
     String name;
     int intoxicated;
-    boolean sleeping = false;
+    boolean awake = true;
     boolean alive = true;
+    boolean parrot = false;
+
 
     Pirates() {
 
     }
 
     void drinkSomeRum() {
-        if (alive) {
+        if (this.alive) {
             this.intoxicated++;
         } else {
             System.out.println(this.name + " dead");
@@ -21,16 +23,46 @@ public class Pirates {
     }
 
     void howsItGoingMate() {
-        if(this.intoxicated < 4) {
-            System.out.println("Pour me anudder!");
+        if (this.alive) {
+            if(this.intoxicated < 4) {
+                System.out.println("Pour me anudder!");
+            } else {
+                System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+            }
+            this.awake = false;
         } else {
-            System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+            System.out.println(this.name + " dead");
         }
-        this.sleeping = true;
+
     }
 
     void die() {
         alive = false;
+    }
+
+
+    void brawl(Pirates pirate) {
+        int a = (int)(Math.random()*100);
+        if (this.alive && pirate.alive) {
+            if (a < 33) {
+                this.alive = false;
+            } else if (a > 66) {
+                pirate.alive = false;
+            } else {
+                this.awake = false;
+                pirate.awake = false;
+            }
+        } else if (this.alive) {
+            System.out.println(pirate.name + "can't brawl, dead");
+        } else if (pirate.alive) {
+            System.out.println(this.name + " can't brawl, dead");
+        } else {
+            System.out.println(pirate.name + this.name + " both dead");
+        }
+    }
+
+    void getParrot(Pirates pirate) {
+        pirate.parrot = true;
     }
 
 
