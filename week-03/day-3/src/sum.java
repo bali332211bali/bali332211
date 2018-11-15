f5753f78d1a999c300d849b8721e2ea573bde6d3
Bali <bali332211@freemail.com>
2018-11-15 11:15:47 +0100

    sum

--- week-03/day-3/src/sum.java
+++ week-03/day-3/src/sum.java
@@ -1,13 +1,19 @@
 public class sum {
 
+    private static int add(int n) {
+
+        if (n / 10 > 0) {
+            return add(n / 10) + n % 10;
+        }
+        return n % 10;
+    }
+
+
 
 
 
     public static void main(String[] args) {
-
-
-
-
+        System.out.println(add(126));
         // Given a non-negative int n, return the sum of its digits recursively (no loops).
         // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while
         // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
