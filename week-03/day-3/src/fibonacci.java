a69626887ce082ad04690ce16178a275e87b4f85
Bali <bali332211@freemail.com>
2018-11-15 17:31:20 +0100

    square

--- week-03/day-3/src/fibonacci.java
+++ week-03/day-3/src/fibonacci.java
@@ -1,31 +1,30 @@
 import java.util.ArrayList;
-import java.util.List;
+    import java.util.List;
 
 public class fibonacci {
 
 
-    private static int number(int n) {
+  private static int number(int n) {
 
-//        List<Integer> numbers = new ArrayList<>();
-//        numbers.add(0);
-//        numbers.add(1);
-
-        if (n > 1) {
-            return number(n-1) + 0 + 0;
-
-        }
-        return n;
-
+    if (n < 1) {
+      return 0;
+    } else if (n < 2) {
+      return 1;
+    } else if (n >= 2) {
+      return number(n-1) + number(n-2);
     }
+    return n;
 
-    public static void main(String[] args) {
-        System.out.println(number(5));
+  }
 
-        // The fibonacci sequence is a famous bit of mathematics, and it happens to
-        // have a recursive definition. The first two values in the sequence are
-        // 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the
-        // previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21
-        // and so on. Define a recursive fibonacci(n) method that returns the nth
-        // fibonacci number, with n=0 representing the start of the sequence.
-    }
+  public static void main(String[] args) {
+    System.out.println(number(7));
+
+    // The fibonacci sequence is a famous bit of mathematics, and it happens to
+    // have a recursive definition. The first two values in the sequence are
+    // 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the
+    // previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21
+    // and so on. Define a recursive fibonacci(n) method that returns the nth
+    // fibonacci number, with n=0 representing the start of the sequence.
+  }
 }
--- week-03/day-3/src/string3.java
+++ week-03/day-3/src/string3.java
@@ -1,18 +1,21 @@
 public class string3 {
 
-    private static String str(String str) {
+  private static String str(String str) {
 
-        if (str.length() > 1) {
-            return str(str.substring(0, str.length()-1)) + "*" + str.charAt(str.length()-1);
-        }
-        return str;
+    if (str.length() > 1) {
+      return str(str.substring(0, str.length()-1)) + "*" + str.charAt(str.length()-1);
     }
+    return str;
+  }
 
 
-    public static void main(String[] args) {
-        System.out.println(str("afsd"));
+  public static void main(String[] args) {
+    System.out.println(str("afsd"));
 
-        // Given a string, compute recursively a new string where all the
-        // adjacent chars are now separated by a "*".
-    }
+    // Given a string, compute recursively a new string where all the
+    // adjacent chars are now separated by a "*".
+  }
+
+
+
 }
