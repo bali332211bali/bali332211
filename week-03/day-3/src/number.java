738eabe8a8bc7139d566a67b573b3377754e711b
Bali <bali332211@freemail.com>
2018-11-15 10:36:38 +0100

    add

--- week-03/day-3/src/number.java
+++ week-03/day-3/src/number.java
@@ -7,8 +7,17 @@ public class number {
         }
     }
 
+    private static void add2(int n) {
+        if (n > 1) {
+            add2(n-1);
+        }
+        System.out.println(n);
+    }
+
     public static void main(String[] args) {
         add(1, 6);
+        System.out.println();
+        add2(6);
 
         // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
     }
