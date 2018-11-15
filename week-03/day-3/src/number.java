19a722aecfa7a90a81e3b44f97d2130b4cc4196b
Bali <bali332211@freemail.com>
2018-11-15 10:57:20 +0100

    add

--- week-03/day-3/src/number.java
+++ week-03/day-3/src/number.java
@@ -1,23 +1,39 @@
 public class number {
 
-    private static void add(int n, int limit) {
-        if (limit >= n) {
-            System.out.println(n);
-            add(n+1, limit);
+
+    private static int add(int n) {
+        if(n == 1) {
+            return 1;
+        } else {
+            return add(n-1) + n;
         }
     }
 
-    private static void add2(int n) {
+
+    private static int add2(int n) {
         if (n > 1) {
-            add2(n-1);
+            return add2(n-1) + n;
+        }
+        return n-1+n;
+    }
+
+    private static void add3(int n) {
+        if (n > 1) {
+            add3(n-1);
         }
         System.out.println(n);
     }
 
     public static void main(String[] args) {
-        add(1, 6);
+        System.out.println(add(6));
         System.out.println();
-        add2(6);
+        System.out.println(add2(6));
+        System.out.println();
+
+        add3(6);
+        System.out.println();
+        System.out.println();
+
 
         // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
     }
