287be9fb5e3dfe828076cf06563763604dfb4a26
Bali <bali332211@freemail.com>
2018-11-29 15:53:34 +0100

    parametric

---
+++ week-05/day-4/src/ParametricAverage.java
@@ -0,0 +1,69 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class ParametricAverage {
+
+
+  static int number() {
+
+    System.out.println("number");
+    Scanner scanner = new Scanner(System.in);
+    int user = scanner.nextInt();
+
+    return user;
+  }
+
+
+
+  public static void main(String[] args) {
+
+
+    HashMap<String, Integer> hashMap = new HashMap<>();
+    List<Integer> numbers = new ArrayList<>();
+
+    try {
+      Path path = Paths.get("src/log.txt");
+      List<String> str = Files.readAllLines(path);
+      Files.lines(path)
+          .forEach(System.out::println);
+    } catch (Exception a) {
+      System.out.println(a);
+    }
+
+    int n = 3;
+    for (int i = 0; i < n; i++) {
+      numbers.add(number());
+    }
+
+    int count = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+      count += numbers.get(i);
+    }
+
+    System.out.println(n + " numbers");
+    System.out.println("sum " + count);
+    System.out.println("average " + (double) count/n);
+    System.out.print("average ");
+    System.out.println(new DecimalFormat("#.##").format((double) count/n));
+
+    for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+      System.out.println(entry);
+    }
+
+
+
+
+
+  }
+
+
+// Write a program that asks for a number.
+// It would ask this many times to enter an integer,
+// if all the integers are entered, it should print the sum and average of these
+// integers like:
+//
+// Sum: 22, Average: 4.4
+}
