0702d85e69a30a68e2ce342523e80f202f373d49
Bali <bali332211@freemail.com>
2018-12-05 16:26:22 +0100

    size

--- week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/WebController.java
+++ week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/WebController.java
@@ -10,16 +10,19 @@ public class WebController {
 
   @RequestMapping("/web/greeting")
   public String greeting(@RequestParam String name, Model model) {
+    Greetings greetings = new Greetings();
 
     String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
         "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
         "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
         "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
-    int n = (int) (Math.random()*hellos.length);
-    Greetings greetings = new Greetings();
-    model.addAttribute("hello", hellos[n]);
+//    int n = ;
+
+    model.addAttribute("hello", hellos[(int) (Math.random()*hellos.length)]);
     model.addAttribute("name", " value");
+    model.addAttribute("color", "orange");
+    model.addAttribute("size", 40);
     model.addAttribute("load", Greetings.id);
     return "greeting";
   }
