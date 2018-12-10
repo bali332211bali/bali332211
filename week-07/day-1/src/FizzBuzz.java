9f68915944f982e4c6d84584df8358efa50a5554
Bali <bali332211@freemail.com>
2018-12-10 10:08:44 +0100

    search

---
+++ week-07/day-1/src/FizzBuzz.java
@@ -0,0 +1,11 @@
+public class FizzBuzz {
+
+  public static String getFizzBuzzValue(int element) {
+    if (element % 3 == 0) {
+      return "Fizz";
+    }
+    return Integer.toString(element);
+  }
+
+
+}
---
+++ week-07/day-1/test/FizzBuzzTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FizzBuzzTest {
+
+
+
+
+  @Test
+  public void test() {
+      assertEquals("Fizz", FizzBuzz.getFizzBuzzValue(3));
+
+  }
+
+
+}
