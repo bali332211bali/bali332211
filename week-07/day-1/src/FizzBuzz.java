bb622115586db843335d4379d0f1617660968858
Bali <bali332211@freemail.com>
2018-12-10 10:18:49 +0100

    fizzbuzz

--- week-07/day-1/src/FizzBuzz.java
+++ week-07/day-1/src/FizzBuzz.java
@@ -1,10 +1,15 @@
 public class FizzBuzz {
 
-  public static String getFizzBuzzValue(int element) {
-    if (element % 3 == 0) {
+  public static String getFizzBuzzValue(int elementOfFizzBuzz) {
+    
+    if (elementOfFizzBuzz % 3 == 0 && elementOfFizzBuzz % 5 == 0) {
+      return "FizzBuzz";
+    } else if (elementOfFizzBuzz % 5 == 0) {
+      return "Buzz";
+    } else if (elementOfFizzBuzz % 3 == 0) {
       return "Fizz";
     }
-    return Integer.toString(element);
+    return Integer.toString(elementOfFizzBuzz);
   }
 
 
--- week-07/day-1/test/FizzBuzzTest.java
+++ week-07/day-1/test/FizzBuzzTest.java
@@ -10,6 +10,8 @@ public class FizzBuzzTest {
   @Test
   public void test() {
       assertEquals("Fizz", FizzBuzz.getFizzBuzzValue(3));
+      assertEquals("Buzz", FizzBuzz.getFizzBuzzValue(5));
+      assertEquals("FizzBuzz", FizzBuzz.getFizzBuzzValue(15));
 
   }
 
