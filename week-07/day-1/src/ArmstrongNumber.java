69c6d62166b422e2a0151bc93aef1aa76a08622f
Bali <bali332211@freemail.com>
2018-12-14 14:37:21 +0100

    armstrong

--- week-07/day-1/src/ArmstrongNumber.java
+++ week-07/day-1/src/ArmstrongNumber.java
@@ -9,10 +9,47 @@ public class ArmstrongNumber {
 
     String[] numberStr = Integer.toString(number).split("");
 
-    int armstrongSum = Arrays.stream(numberStr)
+    int armstrongSum = Arrays
+        .stream(numberStr)
         .mapToInt(str -> (int) (Math.pow(Integer.parseInt(str), numberStr.length)))
         .sum();
 
     return armstrongSum == number;
+
   }
+
+
+  public String[] splitNumberToString(int number) {
+
+    String[] numberStr = Integer.toString(number).split("");
+
+    return numberStr;
+
+  }
+
+
+  public int sumRaisedNumbers(String[] numberStr) {
+
+    int sumOfRaisedNumbers = 0;
+
+    for (int i = 0; i < numberStr.length; i++) {
+      sumOfRaisedNumbers += Math.pow(Integer.parseInt(numberStr[i]), numberStr.length);
+    }
+
+    return sumOfRaisedNumbers;
+
+  }
+
+
+  public static void main(String[] args) {
+    int startNumber = 153;
+
+    if (ArmstrongNumber.isArmstrongNumber(startNumber)) {
+      System.out.println("Armstrong number");
+    } else {
+      System.out.println("not Armstrong number");
+    }
+
+  }
+
 }
