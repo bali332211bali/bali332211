0cb3dde5a436e5496b0c108490d2169660215184
Bali <bali332211@freemail.com>
2018-12-20 15:55:40 +0100

    post

---
+++ week-08/day-1/trending posts/src/main/java/com/greenfoxacademy/springstart/Models/Post.java
@@ -0,0 +1,43 @@
+package com.greenfoxacademy.springstart.Models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  private String title;
+  private String text;
+
+  public Post() {
+
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+}
