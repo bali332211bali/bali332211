ad92bfc767d8dd8af8453d4de5c5c54602849fd5
Bali <bali332211@freemail.com>
2019-01-24 10:50:41 +0100

    requestbody rest part for tourist

--- week-11/tourist rueppellii-orientation-exam-chika/src/main/java/com/greenfoxacademy/tourist/controllers/ApiController.java
+++ week-11/tourist rueppellii-orientation-exam-chika/src/main/java/com/greenfoxacademy/tourist/controllers/ApiController.java
@@ -3,6 +3,8 @@ package com.greenfoxacademy.tourist.controllers;
 import com.greenfoxacademy.tourist.models.Translation;
 import com.greenfoxacademy.tourist.services.TranslationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,8 +28,12 @@ public class ApiController {
   }
 
   @PostMapping("/api/newTranslation")
-  public String newTranslation(@RequestBody()) {
-
+  public ResponseEntity<Translation> newTranslation(@RequestBody Translation translation) {
+    if(translation != null) {
+      translationService.saveTranslation(translation);
+      return new ResponseEntity<>(translation, HttpStatus.OK);
+    }
+    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
   }
 
 }
